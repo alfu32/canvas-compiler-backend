3ea53a9274505f9b499ecbab1c8d348a5480444e
alfu32 <ioan.alferaru@gmail.com>
2023-05-27 07:25:37 +0200

    fix(merge)

--- compilers/js_compilerrrr.v
+++ compilers/js_compilerrrr.v
@@ -2,20 +2,20 @@ module compilers
 
 import geometry
 
-pub struct JsNodeCompiler {
+pub struct JsNodeCompilerrrr {
 pub mut:
 	id string = 'node/javascript'
 }
 
-pub fn (mut c JsNodeCompiler) get_file_name(path []geometry.MetadataRecord) string {
+pub fn (mut c JsNodeCompilerrrr) get_file_name(path []geometry.MetadataRecord) string {
 	return '${get_partial_file_name(path)}.node.js'
 }
 
-pub fn (mut c JsNodeCompiler) get_fq_name(path []geometry.MetadataRecord) string {
+pub fn (mut c JsNodeCompilerrrr) get_fq_name(path []geometry.MetadataRecord) string {
 	return get_partial_fq_name(path)
 }
 
-pub fn (mut c JsNodeCompiler) get_compiled_content(em geometry.MetadataRecord, index map[string]geometry.MetadataRecord) string {
+pub fn (mut c JsNodeCompilerrrr) get_compiled_content(em geometry.MetadataRecord, index map[string]geometry.MetadataRecord) string {
 	simple_name := em.drawable.name
 	ports := get_ports(em, index)
 	lines := em.metadata.text
